64e0e8086a3ca689a648f32980515c9b217b7a82
Tugce Vatansever GUNDUZ <tugcevatansever91@gmail.com>
2023-03-20 16:30:55 +0100

    create placeholder components

---
+++ online-store-app/src/components/ProductList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class ProductList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+    render() {
+        return (
+            <div>
+                <h3>{this.props.info.title}</h3>
+            </div>
+        )
+    }
+}
