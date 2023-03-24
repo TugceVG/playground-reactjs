361a83390f26c98fc9450643e6fe8789688e8cd2
Tugce Vatansever GUNDUZ <tugcevatansever91@gmail.com>
2023-03-24 23:42:11 +0100

    add routing with react-router-dom

--- online-store-app/src/App.js
+++ online-store-app/src/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import { Routes, Route } from "react-router-dom";
+import alertify from 'alertifyjs';
+import { Col, Container, Row } from 'reactstrap';
+
 import CategoryList from './components/CategoryList';
 import Navi from './components/Navi';
 import ProductList from './components/ProductList';
-import { Col, Container, Row } from 'reactstrap';
-import alertify from 'alertifyjs';
+import NotFound from './components/NotFound';
+import CartList from './components/CartList';
 
 export default class App extends Component {
   // This declaration is the old generation
@@ -55,16 +59,27 @@ export default class App extends Component {
     let productInfo = { title: "Product List" };
     let categoryInfo = { title: "Category List" };
     return (
-      <div>
-        {/* This Container and Row components come from reactstrap */}
-        <Container>
-          <Navi removeFromCart={this.removeFromCart} cart={this.state.cart} />
-          <Row>
-            <Col xs="3"><CategoryList currentCategory={this.state.currentCategory} changeCategory={this.changeCategory} info={categoryInfo} /></Col>
-            <Col xs="9"><ProductList addToCart={this.addToCart} products={this.state.products} currentCategory={this.state.currentCategory} info={productInfo} /></Col>
-          </Row>
-        </Container>
-      </div>
+      <Container>
+        <Navi removeFromCart={this.removeFromCart} cart={this.state.cart} />
+        <Row>
+          <Col xs="3"><CategoryList currentCategory={this.state.currentCategory} changeCategory={this.changeCategory} info={categoryInfo} /></Col>
+          <Col xs="9">
+            <Routes>
+              <Route path="/" element={<ProductList
+                addToCart={this.addToCart}
+                products={this.state.products}
+                currentCategory={this.state.currentCategory}
+                info={productInfo}
+              />} />
+              <Route
+                path="/cart"
+                element={<CartList cart={this.state.cart} removeFromCart={this.removeFromCart} />}
+              />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </Col>
+        </Row>
+      </Container>
     )
   }
 }
