80658cc7984bf5508efe0082a9934ccc149c15ac
Tugce Vatansever GUNDUZ <tugcevatansever91@gmail.com>
2023-03-30 22:21:24 +0200

    move and delete files

--- online-store-app/src/index.js
+++ online-store-app/src/index.js
@@ -4,8 +4,8 @@ import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'alertifyjs/build/css/alertify.min.css';
 
-import './index.css';
-import App from './App';
+
+import App from './components/root/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
