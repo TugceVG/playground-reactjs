be41b4d0f2e14b7d0d64060f9403eb2c6c9e39d3
Tugce Vatansever GUNDUZ <tugcevatansever91@gmail.com>
2023-03-30 22:22:31 +0200

    update structure

---
+++ online-store-app/src/components/navi/Navi.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import "./navi.css";
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink
+} from 'reactstrap';
+import { Link } from 'react-router-dom';
+import CartSummary from '../cart/CartSummary';
+
+export default class Navi extends Component {
+    constructor(props) {
+        super(props);
+        this.toggle = this.toggle.bind(this);
+        this.state = { isOpen: false };
+    }
+    toggle() {
+        this.setState({ isOpen: !this.state.isOpen });
+    }
+
+    render() {
+        return (
+            <div>
+                <Navbar color="light" light expand="md">
+                    <NavbarBrand href="/">Online Store App</NavbarBrand>
+                    <NavbarToggler onClick={this.toggle} />
+                    <Collapse isOpen={this.state.isOpen} navbar className="right">
+                        <Nav className="ml-auto" navbar>
+                            <NavItem>
+                                <NavLink>
+                                    <Link to="/form1">Form</Link>
+                                </NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink>
+                                    <Link to="/form2">Form2</Link>
+                                </NavLink>
+                            </NavItem>
+                            <CartSummary removeFromCart={this.props.removeFromCart} cart={this.props.cart} />
+                        </Nav>
+                    </Collapse>
+                </Navbar>
+            </div>
+        )
+    }
+}
