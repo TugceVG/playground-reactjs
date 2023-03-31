79c4f146c5c0fde606be5969c538566e2ad9b871
Tugce Vatansever GUNDUZ <tugcevatansever91@gmail.com>
2023-03-31 23:08:53 +0200

    add redux

---
+++ online-store-app-redux/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './components/root/App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Provider } from "react-redux";
+import configureStore from './redux/reducers/configureStore';
+
+const store = configureStore();
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Provider store={store}> <App /> </Provider >
+);
+
