6975d2935387f5dc9435c2f1b415093736db465f
Tugce Vatansever GUNDUZ <tugcevatansever91@gmail.com>
2023-04-03 23:00:22 +0200

    feat(online-store): add CartSummary component

---
+++ online-store-app-redux/src/components/cart/CartSummary.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from "redux";
+import {
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem,
+    NavItem,
+    NavLink,
+    Badge,
+} from 'reactstrap';
+
+import * as cartActions from '../../redux/actions/cartActions';
+
+class CartSummary extends Component {
+    renderEmpty() {
+        return (
+            <NavItem>
+                <NavLink>Cart is empty</NavLink>
+            </NavItem>
+        )
+    }
+
+    renderSummary() {
+        return (
+            <UncontrolledDropdown nav inNavbar>
+                <DropdownToggle nav caret>
+                    Your Cart
+                </DropdownToggle>
+                <DropdownMenu right>
+                    {
+                        this.props.cart.map((cartItem) => (
+                            <DropdownItem key={cartItem.product.id}>
+                                <Badge color="danger" onClick={() => this.props.actions.removeFromCart(cartItem.product)}>X</Badge>
+                                {cartItem.product.productName}
+                                <Badge color="success">{cartItem.quantity}</Badge>
+                            </DropdownItem>
+                        ))}
+
+                    <DropdownItem divider />
+                    <DropdownItem>Go to Cart</DropdownItem>
+                </DropdownMenu>
+            </UncontrolledDropdown>
+        )
+    }
+
+    render() {
+        return (
+            <div>
+                {
+                    this.props.cart.length > 0 ? this.renderSummary() : this.renderEmpty()
+                }
+            </div>
+        )
+    }
+}
+
+function mapStateToProps(state) {
+    return {
+        cart: state.cartReducer
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        actions: {
+            removeFromCart: bindActionCreators(cartActions.removeFromCart, dispatch),
+        }
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartSummary);
