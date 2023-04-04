00c9ef3ab8540cfcc55ddedaf5129eb7a79d01d9
Tugce Vatansever GUNDUZ <tugcevatansever91@gmail.com>
2023-04-04 16:46:13 +0200

    feat(online-store): add routing

--- online-store-app-redux/src/index.js
+++ online-store-app-redux/src/index.js
@@ -5,10 +5,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'alertifyjs/build/css/alertify.min.css';
 import { Provider } from "react-redux";
 import configureStore from './redux/reducers/configureStore';
+import { BrowserRouter } from "react-router-dom";
 
 const store = configureStore();
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}> <App /> </Provider >
+  <BrowserRouter> <Provider store={store}> <App /> </Provider > </BrowserRouter>
 );
 
