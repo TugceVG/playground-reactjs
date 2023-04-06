a5050a3c5488e8c9d613857605d2b19fe9d3bf14
Tugce Vatansever GUNDUZ <tugcevatansever91@gmail.com>
2023-04-06 22:36:08 +0200

    chore(online-store): update routing

--- online-store-app-redux/src/components/root/App.js
+++ online-store-app-redux/src/components/root/App.js
@@ -5,6 +5,7 @@ import Dashboard from "./Dashboard";
 import Navi from '../navi/Navi'
 import CartDetail from '../cart/CartDetail';
 import NotFound from '../common/NotFound';
+import AddOrUpdateProduct from '../products/AddOrUpdateProduct';
 
 export default class App extends Component {
   render() {
@@ -14,6 +15,7 @@ export default class App extends Component {
         <Routes>
           <Route path="/" element={<Dashboard />} />
           <Route path="/product" element={<Dashboard />} />
+          <Route path="/saveproduct/:productId" element={<AddOrUpdateProduct />} />
           <Route path="/cart" element={<CartDetail />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
