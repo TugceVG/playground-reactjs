42d7db7e6b0ce79b11384fa6d5719f28b00621e1
Tugce Vatansever GUNDUZ <tugcevatansever91@gmail.com>
2023-05-15 23:03:50 +0200

    feat(social-media): create Feed, Post and Share components

---
+++ social-media-app/src/components/feed/Feed.jsx
@@ -0,0 +1,20 @@
+import Post from "../post/Post.jsx";
+import Share from "../share/Share.jsx";
+import "./feed.css";
+
+export default function Feed() {
+    return (
+        <div className="feed">
+            <div className="feedWrapper">
+                <Share />
+                <Post />
+                <Post />
+                <Post />
+                <Post />
+                <Post />
+                <Post />
+                <Post />
+            </div>
+        </div>
+    )
+}
