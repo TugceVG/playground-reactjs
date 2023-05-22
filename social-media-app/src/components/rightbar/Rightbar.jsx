51dbe09d62cb28becaec4aeaf374801a62513fea
Tugce Vatansever GUNDUZ <tugcevatansever91@gmail.com>
2023-05-22 14:04:04 +0200

    chore(social-media): update Rightbar with conditional rendering

--- social-media-app/src/components/rightbar/Rightbar.jsx
+++ social-media-app/src/components/rightbar/Rightbar.jsx
@@ -2,11 +2,11 @@ import "./rightbar.css";
 import { Users } from "../../dummyData";
 import Online from "../online/Online";
 
-export default function Rightbar() {
+export default function Rightbar({ profile }) {
 
-    return (
-        <div className="rightbar">
-            <div className="rightbarWrapper">
+    const HomeRightbar = () => {
+        return (
+            <>
                 <div className="birthdayContainer">
                     <img src="assets/gift.png" className="birthdayImg" alt="" />
                     <span className="birthdayText">
@@ -18,6 +18,63 @@ export default function Rightbar() {
                 <ul className="rightbarFriendList">
                     {Users.map((user) => <Online key={user.id} user={user} />)}
                 </ul>
+            </>
+        )
+    }
+
+    const ProfileRightbar = () => {
+        return (
+            <>
+                <h4 className="rightbarTitle">User information</h4>
+                <div className="rightbarInfo">
+                    <div className="rightbarInfoItem">
+                        <span className="rightbarInfoKey">City:</span>
+                        <span className="rightbarInfoValue">Luxembourg</span>
+                    </div>
+                    <div className="rightbarInfoItem">
+                        <span className="rightbarInfoKey">From:</span>
+                        <span className="rightbarInfoValue">Turkey</span>
+                    </div>
+                    <div className="rightbarInfoItem">
+                        <span className="rightbarInfoKey">Relationship:</span>
+                        <span className="rightbarInfoValue">Married</span>
+                    </div>
+                </div>
+                <h4 className="rightbarTitle">User Friends</h4>
+                <div className="rightbarFollowings">
+                    <div className="rightbarFollowing">
+                        <img src="assets/person/1.jpeg" alt="" className="rightbarFollowingImg" />
+                        <span className="rightbarFollowingName">John Carter</span>
+                    </div>
+                    <div className="rightbarFollowing">
+                        <img src="assets/person/1.jpeg" alt="" className="rightbarFollowingImg" />
+                        <span className="rightbarFollowingName">John Carter</span>
+                    </div>
+                    <div className="rightbarFollowing">
+                        <img src="assets/person/1.jpeg" alt="" className="rightbarFollowingImg" />
+                        <span className="rightbarFollowingName">John Carter</span>
+                    </div>
+                    <div className="rightbarFollowing">
+                        <img src="assets/person/1.jpeg" alt="" className="rightbarFollowingImg" />
+                        <span className="rightbarFollowingName">John Carter</span>
+                    </div>
+                    <div className="rightbarFollowing">
+                        <img src="assets/person/1.jpeg" alt="" className="rightbarFollowingImg" />
+                        <span className="rightbarFollowingName">John Carter</span>
+                    </div>
+                    <div className="rightbarFollowing">
+                        <img src="assets/person/1.jpeg" alt="" className="rightbarFollowingImg" />
+                        <span className="rightbarFollowingName">John Carter</span>
+                    </div>
+                </div>
+            </>
+        )
+    }
+
+    return (
+        <div className="rightbar">
+            <div className="rightbarWrapper">
+                {profile ? <ProfileRightbar /> : <HomeRightbar />}
             </div>
         </div>
     )
