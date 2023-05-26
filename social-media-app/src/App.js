8635ba33786463c8cab62fcd671130fb49b2f7ad
Tugce Vatansever GUNDUZ <tugcevatansever91@gmail.com>
2023-05-26 22:41:39 +0200

    feat(social-media): add react-router-dom

--- social-media-app/src/App.js
+++ social-media-app/src/App.js
@@ -1,9 +1,18 @@
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
+import Profile from "./pages/profile/Profile";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
 
 function App() {
   return (
     <>
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile/:username" element={<Profile />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
     </>
   );
 }
