6ce0d9c881a9ad2e6626bfa11ace891dbf91ba1c
Tugce Vatansever GUNDUZ <tugcevatansever91@gmail.com>
2023-05-31 23:24:53 +0200

    feat(social-media): create context API for authentication

--- social-media-app/src/pages/login/Login.jsx
+++ social-media-app/src/pages/login/Login.jsx
@@ -1,6 +1,18 @@
+import { useRef, useContext } from "react";
+import { loginCall } from "../../apiCalls";
+import { AuthContext } from "../../context/AuthContext";
 import "./login.css";
 
 export default function Login() {
+    const email = useRef();
+    const password = useRef();
+    const { user, isFetching, error, dispatch } = useContext(AuthContext);
+
+    const handleClick = (e) => {
+        e.preventDefault();
+        loginCall({ email: email.current.value, password: password.current.value }, dispatch);
+    }
+    console.log(user)
     return (
         <div className="login">
             <div className="loginWrapper">
@@ -9,14 +21,14 @@ export default function Login() {
                     <span className="loginDesc">Connect with friends and the world around you on TugceSocial.</span>
                 </div>
                 <div className="loginRight">
-                    <div className="loginBox">
-                        <input placeholder="E-mail" type="email" className="loginInput" />
-                        <input placeholder="Password" type="password" className="loginInput" />
+                    <form className="loginBox" onSubmit={handleClick}>
+                        <input placeholder="E-mail" type="email" className="loginInput" ref={email} required />
+                        <input placeholder="Password" type="password" className="loginInput" ref={password} minLength="6" required />
                         <button className="loginButton">Log In</button>
                         <span className="loginForgot">Forgot password?</span>
                         <hr className="loginHr" />
                         <button className="loginRegisterButton">Create a New Account</button>
-                    </div>
+                    </form>
                     <span className="loginRightDesc"><b>Create a Page</b> for a celebrity, brand or business.</span>
                 </div>
             </div>
