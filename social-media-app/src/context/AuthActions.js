3a107cb3e6e4666da9b6834626e452743f8a783f
Tugce Vatansever GUNDUZ <tugcevatansever91@gmail.com>
2023-06-06 23:19:38 +0200

    feat(social-media): add follow action to the context

--- social-media-app/src/context/AuthActions.js
+++ social-media-app/src/context/AuthActions.js
@@ -1,13 +1,23 @@
-export const LoginStart = (user) => ({
+export const LoginStart = () => ({
     type: "LOGIN_START",
 });
 
-export const LoginStart = (user) => ({
+export const LoginSuccess = (user) => ({
     type: "LOGIN_SUCCESS",
     payload: user,
 });
 
-export const LoginStart = (error) => ({
+export const LoginFailure = (error) => ({
     type: "LOGIN_FAILURE",
     payload: error,
 });
+
+export const Follow = (userId) => ({
+    type: "FOLLOW",
+    payload: userId,
+})
+
+export const Unfollow = (userId) => ({
+    type: "UNFOLLOW",
+    payload: userId,
+})
