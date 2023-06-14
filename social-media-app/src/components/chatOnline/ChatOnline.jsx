bb90d3224b83063b15bb9d8cc9af9403c2b33502
Tugce Vatansever GUNDUZ <tugcevatansever91@gmail.com>
2023-06-14 23:27:16 +0200

    feat(social-media): create ChatOnline component

---
+++ social-media-app/src/components/chatOnline/ChatOnline.jsx
@@ -0,0 +1,31 @@
+import "./chatOnline.css";
+
+export default function ChatOnline() {
+    return (
+        <div className="chatOnline">
+            <div className="chatOnlineFriend">
+                <div className="chatOnlineImgContainer">
+                    <img className="chatOnlineImg" src="assets/person/7.jpeg" alt="" />
+                    <div className="chatOnlineBadge"></div>
+                </div>
+                <span className="chatOnlineName">Suleyman Gunduz</span>
+            </div>
+
+            <div className="chatOnlineFriend">
+                <div className="chatOnlineImgContainer">
+                    <img className="chatOnlineImg" src="assets/person/7.jpeg" alt="" />
+                    <div className="chatOnlineBadge"></div>
+                </div>
+                <span className="chatOnlineName">Suleyman Gunduz</span>
+            </div>
+
+            <div className="chatOnlineFriend">
+                <div className="chatOnlineImgContainer">
+                    <img className="chatOnlineImg" src="assets/person/7.jpeg" alt="" />
+                    <div className="chatOnlineBadge"></div>
+                </div>
+                <span className="chatOnlineName">Suleyman Gunduz</span>
+            </div>
+        </div>
+    )
+}
