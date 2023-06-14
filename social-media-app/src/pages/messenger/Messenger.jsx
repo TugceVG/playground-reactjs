8d50583cf1a11b4d5d147878da5d24eb7265f752
Tugce Vatansever GUNDUZ <tugcevatansever91@gmail.com>
2023-06-14 23:53:53 +0200

    feat(social-media): create Messenger page

---
+++ social-media-app/src/pages/messenger/Messenger.jsx
@@ -0,0 +1,44 @@
+import ChatOnline from "../../components/chatOnline/ChatOnline";
+import Conversations from "../../components/conversation/Conversations";
+import Message from "../../components/message/Message";
+import Topbar from "../../components/topbar/Topbar";
+import "./messenger.css";
+
+export default function Messenger() {
+    return (
+        <>
+            <Topbar />
+            <div className="messenger">
+                <div className="chatMenu">
+                    <div className="chatMenuWrapper">
+                        <input className="chatMenuInput" placeholder="Search for friends..." />
+                        <Conversations />
+                        <Conversations />
+                        <Conversations />
+                        <Conversations />
+                    </div>
+                </div>
+                <div className="chatBox">
+                    <div className="chatBoxWrapper">
+                        <div className="chatBoxTop">
+                            <Message />
+                            <Message own={true} />
+                            <Message />
+                            <Message own={true} />
+                            <Message />
+                        </div>
+                        <div className="chatBoxBottom">
+                            <textarea className="chatMessageInput" placeholder="Write something..." ></textarea>
+                            <button className="chatSubmitButton">Send</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="chatOnline">
+                    <div className="chatOnlineWrapper">
+                        <ChatOnline />
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
