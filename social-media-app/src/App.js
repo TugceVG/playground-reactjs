d4dbe95a7f420594bed45bb61fc6f39b76ec5aef
Tugce Vatansever GUNDUZ <tugcevatansever91@gmail.com>
2023-06-27 22:19:08 +0200

    chore(social-media): update route

--- social-media-app/src/App.js
+++ social-media-app/src/App.js
@@ -16,7 +16,7 @@ function App() {
         <Route path="/login" element={user ? <Navigate to="/" /> : <Login />} />
         <Route path="/register" element={user ? <Navigate to="/" /> : <Register />} />
         <Route path="/messenger" element={!user ? <Navigate to="/" /> : <Messenger />} />
-        <Route path="/profile/:username" element={<Profile />} />
+        <Route path="/profile/:username" element={user ? <Profile /> : <Navigate to="/login" />} />
       </Routes>
     </>
   );
