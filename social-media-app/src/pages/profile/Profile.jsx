e5b75ee6bb5936e9e5b96a3482eab36f099bc5a8
Tugce Vatansever GUNDUZ <tugcevatansever91@gmail.com>
2023-07-11 15:40:17 +0200

    chore(social-media): refactor Messenger and Profile styles

--- social-media-app/src/pages/profile/Profile.jsx
+++ social-media-app/src/pages/profile/Profile.jsx
@@ -6,7 +6,7 @@ import Topbar from "../../components/topbar/Topbar";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Feed from "../../components/feed/Feed";
 import Rightbar from "../../components/rightbar/Rightbar";
-import "./profile.css";
+import "./profile.scss";
 
 const PF = process.env.REACT_APP_PUBLIC_FOLDER;
 
