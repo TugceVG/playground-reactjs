34c60d77e5a8112cfa61f40578938847a3ab01b6
Tugce Vatansever GUNDUZ <tugcevatansever91@gmail.com>
2023-07-18 00:42:10 +0200

    fix(slider): next and previous pokemon

--- slider/src/App.js
+++ slider/src/App.js
@@ -3,11 +3,10 @@ import Card from './components/card/Card';
 
 function App() {
   const [pokemons, setPokemons] = useState([]);
-  const [selectedPokemonIndex, setSelectedPokemonIndex] = useState(0);
 
   useEffect(() => {
     function fetchPokemonData(pokemon) {
-      let url = pokemon.url // <--- this is saving the pokemon url to a      variable to us in a fetch.(Ex: https://pokeapi.co/api/v2/pokemon/1/)
+      let url = pokemon.url // <--- this is saving the pokemon url to a variable to us in a fetch.(Ex: https://pokeapi.co/api/v2/pokemon/1/)
       fetch(url)
         .then(response => response.json())
         .then((pokeData) => {
@@ -16,21 +15,21 @@ function App() {
     }
 
     function fetchKantoPokemon() {
-      fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
+      fetch("https://pokeapi.co/api/v2/pokemon?limit=6")
         .then(response => response.json())
         .then(allpokemon => {
           allpokemon.results.forEach(function (pokemon) {
+            console.log(pokemon)
             fetchPokemonData(pokemon);
           })
         });
     }
-
     fetchKantoPokemon();
   }, []);
 
   return (
     <div className="App">
-      <Card pokemon={pokemons[selectedPokemonIndex]} setSelectedPokemonIndex={setSelectedPokemonIndex} />
+      <Card pokemons={pokemons} />
     </div>
   );
 }
--- slider/src/components/card/Card.jsx
+++ slider/src/components/card/Card.jsx
@@ -1,32 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 
 import "./card.scss";
 
-export default function Card({ pokemon, setSelectedPokemonIndex }) {
+export default function Card({ pokemons }) {
+  const [selectedPokemonIndex, setSelectedPokemonIndex] = useState(0);
+  const count = pokemons.length;
 
   const handleIncrease = () => {
-    setSelectedPokemonIndex(prev => prev + 1)
+    if (selectedPokemonIndex >= count - 1) {
+      setSelectedPokemonIndex(0)
+    } else {
+      setSelectedPokemonIndex(prev => prev + 1)
+    }
   }
 
   const handleDecrease = () => {
-    setSelectedPokemonIndex(prev => prev - 1)
+    if (selectedPokemonIndex <= 0) {
+      setSelectedPokemonIndex(count - 1)
+    } else {
+      setSelectedPokemonIndex(prev => prev - 1);
+    }
+  }
+  // console.log("index", selectedPokemonIndex)
+
+  const activePokemon = pokemons[selectedPokemonIndex];
+
+  if (!activePokemon) {
+    return null;
   }
 
   return (
     <div className="card">
-      <ArrowBackIosIcon className="arrow" onClick={handleIncrease} />
+      <ArrowBackIosIcon className="arrow" onClick={handleDecrease} />
       <div className="cardContainer">
         <div className="cardContainerTop">
-          <img src={pokemon?.sprites?.front_shiny} className="cardContainerTopPic" alt="" />
+          <img src={activePokemon.sprites?.front_shiny} className="cardContainerTopPic" alt="" />
         </div>
         <div className="cardContainerBottom">
-          <h3 className="cardContainerBottomHeader">Black Dog</h3>
+          <h3 className="cardContainerBottomHeader">{activePokemon.name}</h3>
           <p>It is a friendly animal </p>
         </div>
       </div>
-      <ArrowForwardIosIcon className="arrow" onClick={handleDecrease} />
+      <ArrowForwardIosIcon className="arrow" onClick={handleIncrease} />
     </div>
   )
 }
