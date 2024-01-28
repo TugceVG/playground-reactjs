a98b8975cc5f949c21727964df530c27ec00d76c
Tugce Vatansever GUNDUZ <tugcevatansever91@gmail.com>
2024-01-28 23:08:15 +0100

    feat(investment-calculator-app): add UserInput component

--- investment-calculator-app/src/App.jsx
+++ investment-calculator-app/src/App.jsx
@@ -1,10 +1,21 @@
 import "./index.css";
 import Header from "./components/Header";
+import UserInput from "./components/UserInput";
+import { useState } from "react";
+
+const INITIAL_INPUTS_VALUES = {
+  initialInvestment: 10000,
+  annualInvestment: 1200,
+  expectedReturn: 6,
+  duration: 10,
+};
 
 function App() {
+  const [userInputs, setUserInputs] = useState(INITIAL_INPUTS_VALUES);
   return (
     <>
       <Header />
+      <UserInput userInputs={userInputs} setUserInputs={setUserInputs} />
     </>
   );
 }
---
+++ investment-calculator-app/src/components/UserInput.jsx
@@ -0,0 +1,53 @@
+export default function UserInput({ userInputs, setUserInputs }) {
+  function handleInputChange(event) {
+    setUserInputs((prev) => ({
+      ...prev,
+      [event.target.name]: event.target.value,
+    }));
+  }
+
+  return (
+    <section id="user-input">
+      <div className="input-group">
+        <p>
+          <label>Initial Investment</label>
+          <input
+            type="number"
+            name="initialInvestment"
+            value={userInputs.initialInvestment}
+            onChange={handleInputChange}
+          />
+        </p>
+        <p>
+          <label>Annual Investment</label>
+          <input
+            type="number"
+            name="annualInvestment"
+            value={userInputs.annualInvestment}
+            onChange={handleInputChange}
+          />
+        </p>
+      </div>
+      <div className="input-group">
+        <p>
+          <label>Expected Return</label>
+          <input
+            type="number"
+            name="expectedReturn"
+            value={userInputs.expectedReturn}
+            onChange={handleInputChange}
+          />
+        </p>
+        <p>
+          <label>Duration</label>
+          <input
+            type="number"
+            name="duration"
+            value={userInputs.duration}
+            onChange={handleInputChange}
+          />
+        </p>
+      </div>
+    </section>
+  );
+}
