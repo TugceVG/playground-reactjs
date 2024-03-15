cbf4d25489e95b53028ae8d41e877a4ce9f3e8a1
Tugce Vatansever GUNDUZ <tugcevatansever91@gmail.com>
2024-03-15 21:21:13 +0100

    feat(mrc-company): add navigation component and basic routes

--- mrc-company/src/App.jsx
+++ mrc-company/src/App.jsx
@@ -1,7 +1,20 @@
 import { Routes, Route } from "react-router-dom";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+import Referances from "./pages/Referances";
+import Services from "./pages/Services";
 
 function App() {
-  return <></>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/services" element={<Services />} />
+      <Route path="/referances" element={<Referances />} />
+      <Route path="/contact" element={<Contact />} />
+    </Routes>
+  );
 }
 
 export default App;
---
+++ mrc-company/src/components/Navbar.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+
+import "./navbar.css";
+import logo from "../assets/logo3.png";
+import { navItems } from "./navItems.js";
+
+function Navbar() {
+  const [isHamburgerMenuOpen, setIsHamburgerMenuOpen] = useState(false);
+  const { pathname } = useLocation();
+
+  const toggleHamburgerMenu = () => setIsHamburgerMenuOpen((prev) => !prev);
+  const closeMobileMenu = () => setClick(false);
+
+  const getSelectedClassName = (navItem) => {
+    const isSelected = navItem.url === pathname;
+    return isSelected ? "nav-item selected" : "nav-item";
+  };
+
+  return (
+    <nav className="navbar">
+      <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+        <img src={logo} />
+      </Link>
+      <div className="menu-icon" onClick={toggleHamburgerMenu}>
+        <i className={isHamburgerMenuOpen ? "fas fa-times" : "fas fa-bars"} />
+      </div>
+      <ul className={isHamburgerMenuOpen ? "nav-menu open" : "nav-menu"}>
+        {navItems.map((navItem) => (
+          <li className={getSelectedClassName(navItem)} key={navItem.id}>
+            <Link
+              to={navItem.url}
+              className="nav-links"
+              onClick={closeMobileMenu}
+            >
+              {navItem.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
