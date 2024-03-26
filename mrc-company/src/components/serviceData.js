dc875d2ef35f61dea9ff19ad21b8d289f345d239
Tugce Vatansever GUNDUZ <tugcevatansever91@gmail.com>
2024-03-26 21:47:10 +0100

    chore(mrc-company): update styles, site context and static assets

--- mrc-company/src/components/serviceData.js
+++ mrc-company/src/components/serviceData.js
@@ -1,7 +1,9 @@
-import image1 from "../assets/2.jpeg";
-import image2 from "../assets/7.jpeg";
-import image3 from "../assets/8.jpeg";
-import image4 from "../assets/10.jpeg";
+import image1 from "../assets/services-img/kat-hizmetleri-4.jpeg";
+import image2 from "../assets/services-img/genel-alan-2.jpeg";
+import image3 from "../assets/services-img/genel-alan-4.jpeg";
+import image4 from "../assets/services-img/mutfak-hizmetleri.jpeg";
+import image5 from "../assets/services-img/fb-2.jpeg";
+import image6 from "../assets/services-img/dis-cephe.jpeg";
 
 export const serviceData = [
   {
@@ -27,9 +29,23 @@ export const serviceData = [
   },
   {
     id: 4,
-    title: "Mobil Ekip",
-    text: "İşyeri temizliği kapsamında lavabolarınız, iş atölyeniz, ofis mutfağınız, yer döşemeleriniz, koltuklarınız, halılarınız, ofis masalarınız, kitaplıklarınız ve camlarınız, uzmanlaşmış geniş kadromuz tarafından titizlikle ve özenle temizlenir.",
+    title: "Mutfak Hizmetleri (Stewarding)",
+    text: "İşletmenizin mutfak operasyonlarını en üst düzeyde yönetmek için buradayız! Stewarding hizmetimizle, temizlik, hijyen ve organizasyon konularında uzman ekibimizle birlikte çalışarak mutfak işlerinizi en yüksek standartlarda sağlıyoruz. Günlük, haftalık veya aylık periyotlarla esnek çözümler sunuyoruz. İşletmenizin mutfak operasyonlarını bize teslim edin ve en üst düzeyde hijyen ve verimlilik sağlayın.",
     src: image4,
+    path: "/services",
+  },
+  {
+    id: 5,
+    title: "Yiyecek ve İçecek (Food & Beverage)",
+    text: "Restoranınızın veya kafenizin başarısı, müşterilerinize sunduğunuz yemeklerle ve hizmet kalitenizle belirlenir. F&B olarak, işletmenizin yiyecek ve içecek operasyonlarını mükemmelleştirmek için buradayız. Deneyimli ekibimiz, lezzetli yemekler hazırlamanıza ve sıcak hizmet sunmanıza yardımcı olur. Müşterilerinizin beklentilerini aşmak için size destek vermek için sabırsızlanıyoruz. Şimdi bize katılın ve müşterilerinizi mutlu etmenin tadını çıkarın.",
+    src: image5,
+    path: "/services",
+  },
+  {
+    id: 6,
+    title: "Dış Cephe Cam Temizliği Hizmeti",
+    text: "Binanızın dış cephesini temiz ve parlak tutun! Profesyonel ekiplerimizle, dış cephe cam temizliği hizmeti sunuyoruz. Modern ekipmanlarımızla, binanızın camlarını kusursuz bir şekilde temizliyoruz. Detaylı bilgi ve randevu için hemen bizimle iletişime geçin.",
+    src: image6,
     path: "/services",
   },
 ];
