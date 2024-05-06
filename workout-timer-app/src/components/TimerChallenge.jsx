3e35af6578700f9033aea40ed1e9538a3db01ab5
Tugce Vatansever GUNDUZ <tugcevatansever91@gmail.com>
2024-05-06 23:47:43 +0200

    feat(workout-timer-app): add TimeChallenge Component and update App

---
+++ workout-timer-app/src/components/TimerChallenge.jsx
@@ -0,0 +1,37 @@
+import React, { useRef, useState } from "react";
+
+export default function TimerChallenge({ title, targetTime }) {
+  const timer = useRef();
+
+  const [timerStarted, settimerStarted] = useState(false);
+  const [timerExpired, setTimerExpired] = useState(false);
+
+  function handleStart() {
+    timer.current = setTimeout(() => {
+      setTimerExpired(true);
+    }, targetTime * 1000);
+    settimerStarted(true);
+  }
+
+  function handleStop() {
+    clearTimeout(timer.current);
+  }
+
+  return (
+    <section className="challenge">
+      <h2>{title}</h2>
+      {timerExpired && <p>You Lost!</p>}
+      <p className="challenge-time">
+        {targetTime} second{targetTime > 1 ? "s" : ""}
+      </p>
+      <p>
+        <button onClick={timerStarted ? handleStop : handleStart}>
+          {timerStarted ? "Stop" : "Start"} Challenge
+        </button>
+      </p>
+      <p className={timerStarted && "active"}>
+        {timerStarted ? "Timer is running..." : "Timer Inactive"}{" "}
+      </p>
+    </section>
+  );
+}
