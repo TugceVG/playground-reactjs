f714146e9a686fa7829e2f4f30aca4c89cb256e2
Tugce <tugcevatansever91@gmail.com>
2024-06-17 20:05:53 +0200

    feat(quiz-app-react): Add Answers and Question components

---
+++ quiz-app-react/src/components/Question.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import Answers from "./Answers";
+import QuestionTimer from "./QuestionTimer";
+import QUESTIONS from "../questions";
+
+export default function Question({
+  questionText,
+  answers,
+  onSelectAnswer,
+  selectedAnswer,
+  answerState,
+  onSkipAnswer,
+}) {
+  return (
+    <div id="question">
+      <QuestionTimer timeout={10000} onTimeout={onSkipAnswer} />
+      <h2>{questionText}</h2>
+      <Answers
+        answers={answers}
+        selectedAnswer={selectedAnswer}
+        answerState={answerState}
+        onSelect={onSelectAnswer}
+      />
+    </div>
+  );
+}
